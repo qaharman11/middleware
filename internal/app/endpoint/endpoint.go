c9b3eea7371360f759a4d1e5ca1919fe1f692c4b
qaharman11 <kenzhekhanovzhanybek@gmail.com>
2023-12-05 18:20:23 +0600

    MiddleWare project

---
+++ internal/app/endpoint/endpoint.go
@@ -0,0 +1,33 @@
+package endpoint
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/labstack/echo"
+)
+
+type Service interface {
+	DaysLeft() int64
+}
+
+type Endpoint struct {
+	s Service
+}
+
+func New(s Service) *Endpoint {
+	return &Endpoint{
+		s: s,
+	}
+}
+
+func (e *Endpoint) Status(c echo.Context) error {
+	d := e.s.DaysLeft()
+	s := fmt.Sprintf("Days left: %d", d)
+
+	err := c.String(http.StatusOK, s)
+	if err != nil {
+		return err
+	}
+	return nil
+}
---
+++ internal/pkg/app/app.go
@@ -0,0 +1,42 @@
+package app
+
+import (
+	"fmt"
+	"log"
+	"middleware/internal/app/endpoint"
+	"middleware/internal/app/mw"
+	"middleware/internal/app/service"
+
+	"github.com/labstack/echo"
+)
+
+type App struct {
+	e    *endpoint.Endpoint
+	s    *service.Service
+	echo *echo.Echo
+}
+
+func New() (*App, error) {
+	a := &App{}
+
+	a.s = service.New()
+	a.e = endpoint.New(a.s)
+
+	a.echo = echo.New()
+
+	a.echo.Use(mw.RoleCheck)
+
+	a.echo.GET("/status", a.e.Status)
+
+	return a, nil
+
+}
+
+func (a *App) Run() error {
+	fmt.Println("server running")
+	err := a.echo.Start(":8080")
+	if err != nil {
+		log.Fatal(err)
+	}
+	return nil
+}
